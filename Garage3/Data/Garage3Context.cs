8693bc32006895d51809183995c655e53d97e5ee
Jörgen Abrahamsson <jorgen@ribbingsfors.se>
2020-05-19 11:28:47 +0200

    Created Project

---
+++ Garage3/Data/Garage3Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Garage3.Models;
+
+namespace Garage3.Data
+{
+    public class Garage3Context : DbContext
+    {
+        public Garage3Context (DbContextOptions<Garage3Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Garage3.Models.Vehicles> Vehicles { get; set; }
+
+        public DbSet<Garage3.Models.Members> Members { get; set; }
+    }
+}
